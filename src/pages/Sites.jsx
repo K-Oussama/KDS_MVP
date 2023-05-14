8f27b97c8409d134700098f66b222db196a2d60f
K-Oussama <oussama_korchi@um5.ac.ma>
2023-05-13 19:39:30 -0600

    update site UI

--- src/pages/Sites.jsx
+++ src/pages/Sites.jsx
@@ -1,10 +1,12 @@
 import Site from '../components/Site';
+import Welcome from '../components/Welcome';
+import LoadingSpinner from '../components/Loadings/LoadingSpinner';
 import { getClientAccessToken, getSites, getSiteById } from '../utils/api';
 import { useEffect, useState } from 'react';
 
 function Sites() {
   const [SiteIds, setSiteIds] = useState([]);
-  const [Sites, setSites] = useState([]);
+  const [sites, setSites] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
@@ -31,17 +33,51 @@ function Sites() {
     }
   }, [SiteIds]);
 
-  console.warn('Sites', Sites)
+  console.warn('Sites', sites)
 
-  if (Sites.length === 0) {
-    return <div>Loading...</div>;
+  if (sites.length === 0) {
+    return (
+      <div className="container my-8 px-6 mx-auto">
+      <Welcome title={"Sites"} description={"Here you can find information about all of the shop's sites."}/>
+      <LoadingSpinner />
+      </div>
+    )
   }
   return (
-    <div>
-      <h1>This is Sites page.</h1>
-      {Sites.map((site) => (
-      <Site key={site.id} site={site} />
-      ))}
+    <div className="container my-8 px-6 mx-auto">
+      <Welcome title={"Sites"} description={"Here you can find information about all of the shop's sites."}/>
+      <br/><br/>
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                  <tr>
+                      <th scope="col" className="px-6 py-3">
+                          Site ID
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                          Site name
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                          creation date
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                          last modified
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                          status
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                          cartridges
+                      </th>
+                  </tr>
+              </thead>
+              <tbody>
+              {sites.map((site) => (
+                  <Site key={site.id} site={site} />
+              ))}
+              </tbody>
+          </table>
+      </div>
     </div>
   );
 }
